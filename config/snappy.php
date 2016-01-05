c5fe00469aee69321b743334cfd98b7c3608f542
Brian <leehongjie91@gmail.com>
2016-01-06 02:45:53 +0800

    patch 6-Jan-2016

--- config/snappy.php
+++ config/snappy.php
@@ -9,9 +9,9 @@ return array(
         // 'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => array(
-                            'print-media-type' => true,
+                            // 'print-media-type' => true,
                             'zoom' => 1.3,
-                            'outline' => true,
+                            // 'outline' => true,
                             'dpi' => 96,
                             'page-size' => 'A4',
                         ),
