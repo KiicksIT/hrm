124dc53ec821436dd68c8366a1733a7d04792137
Brian <leehongjie91@gmail.com>
2016-01-06 02:25:07 +0800

    patch 6-Jan-2016

--- config/snappy.php
+++ config/snappy.php
@@ -6,8 +6,15 @@ return array(
     'pdf' => array(
         'enabled' => true,
         'binary' => '/usr/local/bin/wkhtmltopdf.sh',
+        // 'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
-        'options' => array(),
+        'options' => array(
+                            'print-media-type' => true,
+                            'zoom' => 1.3,
+                            'outline' => true,
+                            'dpi' => 96,
+                            'page-size' => 'A4',
+                        ),
     ),
     'image' => array(
         'enabled' => true,
