3daf99eccbe29d723078f574ed705c7335e207ef
Brian <leehongjie91@gmail.com>
2016-02-25 19:23:47 +0800

    patch 25-Feb-2016

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+// dude
 // use App\Permission;
 
 class AuthServiceProvider extends ServiceProvider
