be5d91c4c94d4ca98d56833c92ca6d342bf85d26
Brian <leehongjie91@gmail.com>
2016-07-15 13:04:19 +0800

    print pdf css formating paydate_paymode

--- resources/views/payslip/printpdf_ch.blade.php
+++ resources/views/payslip/printpdf_ch.blade.php
@@ -185,7 +185,7 @@
                     </div>
                 </div>
 
-                <div class="col-xs-6 row">
+                <div class="col-xs-6 row pull-right">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h3 class="panel-title"><strong>Mode of Payment 支付方式</strong></h3>
