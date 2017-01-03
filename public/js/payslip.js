44e4f28d5e18d3bd86585d90962c80f1a71a1581
Brian Lee Hong Jie <Brian@Brians-MacBook-Air.local>
2017-01-03 15:59:36 +0800

    script add angular ready

--- public/js/payslip.js
+++ public/js/payslip.js
@@ -6,35 +6,36 @@ var app = angular.module('app', ['ui.bootstrap', 'angularUtils.directives.dirPag
         $scope.people = '';
         $scope.months = [];
 
-        $http.get('/api/months').success(function(response) {
-            for (i = 0; i < response.length; i++){
-                $scope.months.push({
-                    id: response[i].id, name: response[i].name, year: moment().year()-1
-                });
-            }
-            for (i = 0; i < response.length; i++){
-                $scope.months.push({
-                    id: response[i].id, name: response[i].name, year: moment().year()
-                });
-            }
-            for (i = 0; i < response.length; i++){
-                $scope.months.push({
-                    id: response[i].id, name: response[i].name, year: moment().year()+1
-                });
-            }
-        });
-
-        $scope.onMonthSelected = function (months){
-            var timeline = months.split('-', 2);
-            var month = parseInt(timeline[0]);
-            var year = parseInt(timeline[1]);
-
-            $http.get('/person/createData/' + month + '/' + year).success(function(people){
-                $scope.people = people;
-            });
-        }
-
         angular.element(document).ready(function () {
+
+            $http.get('/api/months').success(function(response) {
+                for (i = 0; i < response.length; i++){
+                    $scope.months.push({
+                        id: response[i].id, name: response[i].name, year: moment().year()-1
+                    });
+                }
+                for (i = 0; i < response.length; i++){
+                    $scope.months.push({
+                        id: response[i].id, name: response[i].name, year: moment().year()
+                    });
+                }
+                for (i = 0; i < response.length; i++){
+                    $scope.months.push({
+                        id: response[i].id, name: response[i].name, year: moment().year()+1
+                    });
+                }
+            });
+
+            $scope.onMonthSelected = function (months){
+                var timeline = months.split('-', 2);
+                var month = parseInt(timeline[0]);
+                var year = parseInt(timeline[1]);
+
+                $http.get('/person/createData/' + month + '/' + year).success(function(people){
+                    $scope.people = people;
+                });
+            }
+
             $('.select').select2({
                 placeholder: 'Select...',
                 allowClear: true
